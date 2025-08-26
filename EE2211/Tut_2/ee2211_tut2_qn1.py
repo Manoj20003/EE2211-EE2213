971e2db3a1edcc3995da9c309c4fb1398e2689e9
Mendu Ram Manoj <154521806+Manoj20003@users.noreply.github.com>
2025-08-26 05:40:10 +0000

    ee2211 tut 2 qn 2

---
+++ EE2211/Tut_2/ee2211_tut2_qn1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_data(file_path):
+    df = pd.read_csv(file_path)
+
+    df.set_index("DataSeries", inplace=True)
+    
+
+    expenditure = df.loc["Total Government Expenditure On Education"]
+
+    
+  
+
+    plt.plot(expenditure.index, expenditure.values)
+    plt.xlabel("Year")
+    plt.ylabel("Expenditure")
+    plt.title("Total Government Expenditure On Education")
+    plt.show()
+    plt.savefig("expenditure_plot2.png")
+    
+
+
+plot_data('GovernmentExpenditureOnEducationAnnual.csv')
---
+++ EE2211/Tut_2/ee2211_tut2_qn2.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+from matplotlib.ticker import MaxNLocator
+
+
+def plot_data(file_path):
+    df = pd.read_csv(file_path)
+
+    df["year"] = df["year"].round().astype(int)
+    df.set_index("year", inplace=True)
+
+    
+    omnibuses = df[df["type"] == "Omnibuses"]
+    excursion_buses = df[df["type"] == "Excursion buses"]
+    private_buses = df[df["type"] == "Private buses"]
+    
+    start_year = df.index.min()
+    end_year = df.index.max()
+    xticks = list(range(start_year, end_year + 1, 2))
+
+    plt.plot(omnibuses.index, omnibuses["number"], label="Omnibuses")
+    plt.plot(excursion_buses.index, excursion_buses["number"], label="Excursion buses")
+    plt.plot(private_buses.index, private_buses["number"], label="Private buses")
+
+    plt.xticks(xticks)
+
+    plt.title("Annual Motor Vehicle Population by Vehicle Type")
+    plt.xlabel("Year")
+    plt.ylabel("Number")
+    plt.legend()
+    plt.savefig("qn_2.png")
+    plt.show()
+    
+
+
+plot_data("AnnualMotorVehiclePopulationbyVehicleType.csv")
+
+
+
+
